ca36a82bcf0ed47125ce9b69f1fc5a3bea70d0b8
Pavel Zenkin <pavel.zenkin@midhard.com>
2024-06-24 18:02:18 +0400

    Сделана черновая верстка

---
+++ Assets/Code/UI/Utilities/ScrollRectReloader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI.Utilities
+{
+    /// <summary>
+    /// Обновление позиции скроллбара после загрузки контента
+    /// </summary>
+    public class ScrollRectReloader : MonoBehaviour
+    {
+        [SerializeField] private ScrollRect _scrollRect;
+
+        private void OnEnable()
+        {
+            Reload();
+        }
+
+        public void Reload()
+        {
+            _scrollRect.content.anchoredPosition = new Vector2(_scrollRect.content.sizeDelta.x / 2f, 0f);
+        }
+    }
+}
---
+++ Assets/Code/UI/Utilities/TextMeshProCorrectScaler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI.Utilities
+{
+    /// <summary>
+    /// Более грамотное изменение размера
+    /// </summary>
+    public class TextMeshProCorrectScaler : MonoBehaviour
+    {
+        [SerializeField] private LayoutElement _spaceLeft;
+        [SerializeField] private LayoutElement _spaceRight;
+        [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
+        [SerializeField] private float _defaultFontSize = 60f;
+        [SerializeField] private float _defaultPreferredWidth = 100000f;
+        
+        private string _text;
+
+        public void Update()
+        {
+            if (_text != _textMeshProUGUI.text)
+            {
+                StopAllCoroutines();
+                StartCoroutine(UpdateTextScale());
+                _text = _textMeshProUGUI.text;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            StopAllCoroutines();
+        }
+
+        private IEnumerator UpdateTextScale()
+        {
+            if (_textMeshProUGUI.fontSize < _defaultFontSize && (_spaceLeft.preferredWidth > 5f || _text.Length > _textMeshProUGUI.text.Length))
+            {
+                _spaceLeft.preferredWidth = _defaultPreferredWidth;
+                _spaceRight.preferredWidth = _defaultPreferredWidth;
+
+                while (_textMeshProUGUI.fontSize < _defaultFontSize && _spaceLeft.preferredWidth > 5f)
+                {
+                    _spaceLeft.preferredWidth /= 10f;
+                    _spaceRight.preferredWidth /= 10f;
+
+                    yield return null;
+                }
+            }
+        }
+    }
+}
